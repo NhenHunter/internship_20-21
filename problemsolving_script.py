8b92cb2739e2fa74ac9d79240ba0c02f2de9b639
Nhen Hunter <nhenhunter@EDUC-LAP-362.local>
2021-01-19 14:50:38 -0500

    example sequence data

--- problemsolving_script.py
+++ problemsolving_script.py
@@ -1,27 +1,32 @@
 #!/usr/bin/env python3
-#create dictionary
+#import needed packages
 import io
 import re
-#find MFI in dictionary and store in variable
+
 with open ("lyrics.txt", "r") as in_handle:
+#read the file and store it in a variable, then remove spaces	
 	lyrics = in_handle.read()
 	lyrics = lyrics.replace(" ","")
-#import needed packages
 print(lyrics)
-#create list of characters stored based on max key (MFI)
+
+#create dictionary
 all_char = {}
-#read the file and store it in a variable, then remove spaces
+
+#iterate over variable
 for item in lyrics:
+#add values and keys to dictionary, increasing each time character is iterated over	
 	if item in all_char:
 		all_char[item] += 1
 	else:
 		all_char[item] = 1
 
 print(str(all_char))
-#iterate over variable
-#add values and keys to dictionary, increasing each time character is iterated over
-key_max = max(all_char, key = all_char.get)
-print(key_max)
 
+#find MFI in dictionary and store in variable
+key_max = max(all_char, key = all_char.get)
+
+print(key_max)
+#create list of characters stored based on max key (MFI)
 final = re.split(key_max, lyrics)
+
 print(final)
